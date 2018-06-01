7c5c2b8923c62aa0d2f08af41b99c3656b779b8f
serxa <sergiose2010@gmail.com>
2018-06-01 09:06:33 +0200

    Creamos las 3 clases del modelo vista controlador
    Diseñamos la interfaz

--- src/comercio/Comercio.java
+++ src/comercio/Comercio.java
@@ -1,29 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package comercio;
 
-import javax.swing.JOptionPane;
-
-/**
- *
- * @author jose
- */
 public class Comercio {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-         Ticket ticket;
-        Arrays arrays1=new Arrays();
-        ticket=arrays1.crearTicket("2");
-        System.out.println(ticket.toString());
-        arrays1.utilizarNomePrecio(JOptionPane.showInputDialog("inserte o numero de referencia"));
-        System.out.println(Arrays.nomeProducto+" "+Arrays.precioProducto);
+
+        Interfaz I = new Interfaz();
+        I.setVisible(true);
+
         
     }
     
