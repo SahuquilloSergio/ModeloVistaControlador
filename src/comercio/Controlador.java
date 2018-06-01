38dbb31634826aaae75fc99d8794429d3f965582
serxa <sergiose2010@gmail.com>
2018-06-01 09:14:57 +0200

    Instanciamos el metodo desconectar y lo comentamos

--- src/comercio/Controlador.java
+++ src/comercio/Controlador.java
@@ -11,6 +11,10 @@ public class Controlador {
     String url = "BaseComercio.db";
     Connection c = null;
     
+    
+    /*
+    Metodo conectar que nos permitira conectarnos a la base
+    */
     public void connectar(){
           try {
             c = DriverManager.getConnection("jdbc:sqlite:"+url);
@@ -19,4 +23,17 @@ public class Controlador {
             Logger.getLogger(Modelo.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    
+    /*
+    Metodo desconectar para desconectarnos de la base
+    */
+    public void desconectar(){
+             try {
+            c.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Modelo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
+
